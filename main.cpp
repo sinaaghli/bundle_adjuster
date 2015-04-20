6afa1d25009c297147f98f7b1be40a90130be5ac
Sina Aghli <Sina@Sinas-MacBook-Pro.local>
2015-04-20 00:10:00 -0600

    BALarge tested with LadyBugDataset successfully

--- main.cpp
+++ main.cpp
@@ -25,27 +25,29 @@
 #include "GLPathRel.h"
 #include "GLPathAbs.h"
 
-std::deque<std::tuple<Eigen::Vector3d, Eigen::Vector3d, double> >   filter;
+#include "BALProblem.h"
 
-int main(int argc, char** argv)
-{
+std::deque<std::tuple<Eigen::Vector3d, Eigen::Vector3d, double> > filter;
+
+int main(int argc, char** argv) {
   std::cout << "Starting bundle_adjuster ..." << std::endl;
 
   GetPot cl_args(argc, argv);
-  //int frame_skip  = cl_args.follow(0, "-skip");
+  // int frame_skip  = cl_args.follow(0, "-skip");
 
   ///----- Set up GUI.
   pangolin::CreateGlutWindowAndBind("bundle_adjuster", 1600, 800);
 
   // Set up panel.
   const unsigned int panel_size = 180;
-  pangolin::CreatePanel("ui").SetBounds(0, 1, 0, pangolin::Attach::Pix(panel_size));
-  pangolin::Var<bool>           ui_camera_follow("ui.Camera Follow", false, true);
-  pangolin::Var<bool>           ui_reset("ui.Reset", true, false);
-  pangolin::Var<bool>           ui_show_vo_path("ui.Show VO Path", true, true);
-  pangolin::Var<bool>           ui_show_ba_path("ui.Show BA Path", false, true);
-  pangolin::Var<bool>           ui_show_ba_rel_path("ui.Show BA Rel Path", true, true);
-  pangolin::Var<bool>           ui_show_gt_path("ui.Show GT Path", true, true);
+  pangolin::CreatePanel("ui")
+      .SetBounds(0, 1, 0, pangolin::Attach::Pix(panel_size));
+  pangolin::Var<bool> ui_camera_follow("ui.Camera Follow", false, true);
+  pangolin::Var<bool> ui_reset("ui.Reset", true, false);
+  pangolin::Var<bool> ui_show_vo_path("ui.Show VO Path", true, true);
+  pangolin::Var<bool> ui_show_ba_path("ui.Show BA Path", false, true);
+  pangolin::Var<bool> ui_show_ba_rel_path("ui.Show BA Rel Path", true, true);
+  pangolin::Var<bool> ui_show_gt_path("ui.Show GT Path", true, true);
 
   // Set up container.
   pangolin::View& container = pangolin::CreateDisplay();
@@ -74,9 +76,8 @@ int main(int argc, char** argv)
   pangolin::View view_3d;
 
   pangolin::OpenGlRenderState stacks3d(
-        pangolin::ProjectionMatrix(640, 480, 420, 420, 320, 240, 1E-3, 10*1000),
-        pangolin::ModelViewLookAt(-5, 0, -8, 0, 0, 0, pangolin::AxisNegZ)
-        );
+      pangolin::ProjectionMatrix(640, 480, 420, 420, 320, 240, 1E-3, 10 * 1000),
+      pangolin::ModelViewLookAt(-5, 0, -8, 0, 0, 0, pangolin::AxisNegZ));
 
   view_3d.SetHandler(new SceneGraph::HandlerSceneGraph(gl_graph, stacks3d))
       .SetDrawFunction(SceneGraph::ActivateDrawFunctor(gl_graph, stacks3d));
@@ -84,8 +85,8 @@ int main(int argc, char** argv)
   container.AddDisplay(view_3d);
 
   // GUI aux variables.
-  bool paused       = true;
-  bool step_once    = false;
+  bool paused = true;
+  bool step_once = false;
 
   ///----- Load file of ground truth poses (optional).
   bool have_gt;
@@ -93,7 +94,9 @@ int main(int argc, char** argv)
   {
     std::string pose_file = cl_args.follow("", "-poses");
     if (pose_file.empty()) {
-      std::cerr << "- NOTE: No poses file given. Not comparing against ground truth!" << std::endl;
+      std::cerr
+          << "- NOTE: No poses file given. Not comparing against ground truth!"
+          << std::endl;
       have_gt = false;
       ui_show_gt_path = false;
     } else {
@@ -107,16 +110,20 @@ int main(int argc, char** argv)
         std::cout << "- NOTE: File is being read in VISION frame." << std::endl;
       } else if (cl_args.search("-C")) {
         // Custom convention.
-        std::cout << "- NOTE: File is being read in *****CUSTOM***** frame." << std::endl;
+        std::cout << "- NOTE: File is being read in *****CUSTOM***** frame."
+                  << std::endl;
       } else if (cl_args.search("-T")) {
         // Tsukuba convention.
-        std::cout << "- NOTE: File is being read in TSUKUBA frame." << std::endl;
+        std::cout << "- NOTE: File is being read in TSUKUBA frame."
+                  << std::endl;
       } else {
         // Robotics convention (default).
-        std::cout << "- NOTE: File is being read in ROBOTICS frame." << std::endl;
+        std::cout << "- NOTE: File is being read in ROBOTICS frame."
+                  << std::endl;
       }
 
-      while (fscanf(fd, "%f\t%f\t%f\t%f\t%f\t%f", &x, &y, &z, &p, &q, &r) != EOF) {
+      while (fscanf(fd, "%f\t%f\t%f\t%f\t%f\t%f", &x, &y, &z, &p, &q, &r) !=
+             EOF) {
         pose(0) = x;
         pose(1) = y;
         pose(2) = z;
@@ -127,9 +134,7 @@ int main(int argc, char** argv)
         Sophus::SE3d T(SceneGraph::GLCart2T(pose));
 
         // Robotics convention (default).
-        poses.push_back(calibu::ToCoordinateConvention(T,
-                                        calibu::RdfRobotics));
-
+        poses.push_back(calibu::ToCoordinateConvention(T, calibu::RdfRobotics));
       }
       std::cout << "- NOTE: " << poses.size() << " poses loaded." << std::endl;
       fclose(fd);
@@ -139,7 +144,7 @@ int main(int argc, char** argv)
 
   ///----- Register callbacks.
   // Hide/Show panel.
-  pangolin::RegisterKeyPressCallback('~', [&](){
+  pangolin::RegisterKeyPressCallback('~', [&]() {
     static bool fullscreen = true;
     fullscreen = !fullscreen;
     if (fullscreen) {
@@ -151,42 +156,41 @@ int main(int argc, char** argv)
   });
 
   // Container view handler.
-  const char keyShowHide[] = {'1','2','3','4','5','6','7','8','9','0'};
-  const char keySave[]     = {'!','@','#','$','%','^','&','*','(',')'};
+  const char keyShowHide[] = {'1', '2', '3', '4', '5', '6', '7', '8', '9', '0'};
+  const char keySave[] = {'!', '@', '#', '$', '%', '^', '&', '*', '(', ')'};
   for (size_t ii = 0; ii < container.NumChildren(); ii++) {
-    pangolin::RegisterKeyPressCallback(keyShowHide[ii], [&container,ii]() {
-      container[ii].ToggleShow(); });
-    pangolin::RegisterKeyPressCallback(keySave[ii], [&container,ii]() {
-      container[ii].SaveRenderNow("screenshot", 4); });
+    pangolin::RegisterKeyPressCallback(
+        keyShowHide[ii], [&container, ii]() { container[ii].ToggleShow(); });
+    pangolin::RegisterKeyPressCallback(keySave[ii], [&container, ii]() {
+      container[ii].SaveRenderNow("screenshot", 4);
+    });
   }
 
   pangolin::RegisterKeyPressCallback(' ', [&paused] { paused = !paused; });
-  pangolin::RegisterKeyPressCallback(pangolin::PANGO_SPECIAL + pangolin::PANGO_KEY_RIGHT,
-                                     [&step_once] {
-                                        step_once = !step_once; });
+  pangolin::RegisterKeyPressCallback(
+      pangolin::PANGO_SPECIAL + pangolin::PANGO_KEY_RIGHT,
+      [&step_once] { step_once = !step_once; });
   pangolin::RegisterKeyPressCallback(pangolin::PANGO_CTRL + 'r',
-                                     [&ui_reset] {
-                                        ui_reset = true; });
+                                     [&ui_reset] { ui_reset = true; });
 
   ///----- Init general variables.
-  unsigned int                      frame_index;
-  Sophus::SE3d                      vo_pose;
-  Sophus::SE3d                      ba_accum_rel_pose;
-  Sophus::SE3d                      ba_global_pose;
+  unsigned int frame_index;
+  Sophus::SE3d vo_pose;
+  Sophus::SE3d ba_accum_rel_pose;
+  Sophus::SE3d ba_global_pose;
 
   // IMU-Camera transform through robotic to vision conversion.
   Sophus::SE3d Trv;
   Trv.so3() = calibu::RdfRobotics;
-  //Sophus::SE3d Tic = rig.t_wc_[0] * Trv;
+  // Sophus::SE3d Tic = rig.t_wc_[0] * Trv;
 
   /////////////////////////////////////////////////////////////////////////////
   ///---- MAIN LOOP
   ///
+  bool use_cerese_ba = true;
   while (!pangolin::ShouldQuit()) {
-
     ///----- Init reset ...
     if (pangolin::Pushed(ui_reset)) {
-
       // Reset GUI path.
       path_gt_vec.clear();
 
@@ -198,16 +202,49 @@ int main(int argc, char** argv)
       ba_global_pose = Sophus::SE3d();
       ba_accum_rel_pose = Sophus::SE3d();
       if (have_gt) {
-        for (int jj=0 ; jj<(int)poses.size() ; jj++ )
+        for (int jj = 0; jj < (int)poses.size(); jj++)
           path_gt_vec.push_back(poses[jj]);
         std::cout << "have_gt is true" << std::endl;
       }
       frame_index++;
 
+      //////////////////////////////////////////////////////////////////////////
+      ///---- Solve BA Problem
+      if (use_cerese_ba) {
+        BALProblem bal_problem;
+        std::string bal_file = cl_args.follow("", "-dataset");
+        if (!bal_problem.LoadFile(bal_file.c_str())) {
+          std::cerr << "ERROR: unable to open file" << bal_file.c_str()
+                    << std::endl;
+          return 0;
+        }
+        const double* observations = bal_problem.observations();
+
+        ceres::Problem problem;
+        for (int ii = 0; ii < bal_problem.num_observations(); ++ii) {
+          ceres::CostFunction* cost_function = SnavelyReprojectionError::Create(
+              observations[2 * ii + 0], observations[2 * ii + 1]);
+          problem.AddResidualBlock(
+              cost_function, NULL,
+              bal_problem.mutable_camera_for_observation(ii),
+              bal_problem.mutable_point_for_observation(ii));
+        }
+        ceres::Solver::Options options;
+        options.linear_solver_type = ceres::DENSE_SCHUR;
+        options.minimizer_progress_to_stdout = true;
+
+        ceres::Solver::Summary summary;
+        ceres::Solve(options, &problem, &summary);
+        std::cout << summary.FullReport() << std::endl;
+        return 0;
+
+      } else {
+        std::cerr << "local ba implementation is being called" << std::endl;
+        return 0;
+      }
     }
 
-
-    /////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////
     ///---- Render
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
@@ -219,7 +256,7 @@ int main(int argc, char** argv)
 
     // Sleep a bit if paused.
     if (paused) {
-      usleep(1e6/60.0);
+      usleep(1e6 / 60.0);
     }
 
     pangolin::FinishFrame();
