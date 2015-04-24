2e5146efebd6e079916b2a59ccbb6962d0ccac14
Sina Aghli <Sina@engr2-15-132-dhcp.int.colorado.edu>
2015-04-24 12:17:45 -0600

    added feature matecher to main

--- BALProblem.h
+++ BALProblem.h
@@ -38,6 +38,8 @@
 #include <cmath>
 #include <cstdio>
 #include <iostream>
+#include <math.h>
+#include <Eigen/Dense>
 
 #include "ceres/ceres.h"
 #include "ceres/rotation.h"
@@ -52,10 +54,10 @@ class BALProblem {
     delete[] parameters_;
   }
 
-  int num_observations()       const { return num_observations_;               }
-  const double* observations() const { return observations_;                   }
-  double* mutable_cameras()          { return parameters_;                     }
-  double* mutable_points()           { return parameters_  + 9 * num_cameras_; }
+  int num_observations() const { return num_observations_; }
+  const double* observations() const { return observations_; }
+  double* mutable_cameras() { return parameters_; }
+  double* mutable_points() { return parameters_ + 9 * num_cameras_; }
 
   double* mutable_camera_for_observation(int i) {
     return mutable_cameras() + camera_index_[i] * 9;
@@ -85,7 +87,7 @@ class BALProblem {
       FscanfOrDie(fptr, "%d", camera_index_ + i);
       FscanfOrDie(fptr, "%d", point_index_ + i);
       for (int j = 0; j < 2; ++j) {
-        FscanfOrDie(fptr, "%lf", observations_ + 2*i + j);
+        FscanfOrDie(fptr, "%lf", observations_ + 2 * i + j);
       }
     }
 
@@ -96,8 +98,8 @@ class BALProblem {
   }
 
  private:
-  template<typename T>
-  void FscanfOrDie(FILE *fptr, const char *format, T *value) {
+  template <typename T>
+  void FscanfOrDie(FILE* fptr, const char* format, T* value) {
     int num_scanned = fscanf(fptr, format, value);
     if (num_scanned != 1) {
       LOG(FATAL) << "Invalid UW data file.";
@@ -124,8 +126,7 @@ struct SnavelyReprojectionError {
       : observed_x(observed_x), observed_y(observed_y) {}
 
   template <typename T>
-  bool operator()(const T* const camera,
-                  const T* const point,
+  bool operator()(const T* const camera, const T* const point,
                   T* residuals) const {
     // camera[0,1,2] are the angle-axis rotation.
     T p[3];
@@ -139,14 +140,14 @@ struct SnavelyReprojectionError {
     // Compute the center of distortion. The sign change comes from
     // the camera model that Noah Snavely's Bundler assumes, whereby
     // the camera coordinate system has a negative z axis.
-    T xp = - p[0] / p[2];
-    T yp = - p[1] / p[2];
+    T xp = -p[0] / p[2];
+    T yp = -p[1] / p[2];
 
     // Apply second and fourth order radial distortion.
     const T& l1 = camera[7];
     const T& l2 = camera[8];
-    T r2 = xp*xp + yp*yp;
-    T distortion = T(1.0) + r2  * (l1 + l2  * r2);
+    T r2 = xp * xp + yp * yp;
+    T distortion = T(1.0) + r2 * (l1 + l2 * r2);
 
     // Compute final projected point position.
     const T& focal = camera[6];
@@ -165,7 +166,7 @@ struct SnavelyReprojectionError {
   static ceres::CostFunction* Create(const double observed_x,
                                      const double observed_y) {
     return (new ceres::AutoDiffCostFunction<SnavelyReprojectionError, 2, 9, 3>(
-                new SnavelyReprojectionError(observed_x, observed_y)));
+        new SnavelyReprojectionError(observed_x, observed_y)));
   }
 
   double observed_x;
--- main.cpp
+++ main.cpp
@@ -5,9 +5,13 @@
 #include <unistd.h>
 #include <deque>
 #include <fstream>
+#include <vector>
 #include "BALProblem.h"
 #include <opencv2/opencv.hpp>
-
+#include <opencv2/core/core.hpp>
+#include <opencv2/features2d/features2d.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/nonfree/features2d.hpp>
 #ifdef WITH_GUI
   #include <pangolin/pangolin.h>
   #include <SceneGraph/SceneGraph.h>
@@ -22,6 +26,69 @@
   #include <HAL/Camera/CameraDevice.h>
   #include <HAL/IMU/IMUDevice.h>
 #endif
+
+void DrawLandmarkCorrespondance(cv::Mat& left_img, const cv::Point left_lm,
+                               cv::Mat& right_img, const cv::Point& right_lm) {
+  const int gap_btw_images = 5;
+  cv::Point tmp_p;
+  tmp_p.x = right_lm.x + 640 + gap_btw_images;
+  tmp_p.y = right_lm.y;
+  cv::line(left_img, left_lm, tmp_p, cv::Scalar(0, 255, 0), 1);
+  tmp_p.x = left_lm.x - 640;
+  tmp_p.y = left_lm.y;
+  cv::line(right_img, tmp_p, right_lm, cv::Scalar(0, 255, 0), 1);
+}
+
+bool FindAndDrawMatches(cv::Mat& left_img, cv::Mat& right_img) {
+
+  //-- Step 1: Detect the keypoints using SIFT Detector
+  int minHessian = 400;
+  cv::SiftFeatureDetector detector( minHessian );
+  std::vector<cv::KeyPoint> keypoints_left, keypoints_right;
+  detector.detect( left_img, keypoints_left );
+  detector.detect( right_img, keypoints_right );
+
+  //-- Step 2: Calculate descriptors (feature vectors)
+  cv::SiftDescriptorExtractor extractor;
+  cv::Mat descriptors_left, descriptors_right;
+  extractor.compute( left_img, keypoints_left, descriptors_left );
+  extractor.compute( right_img, keypoints_right, descriptors_right );
+
+  //-- Step 3: Matching descriptor vectors using FLANN matcher
+  // TODO(sina) what is flann matcher
+  cv::FlannBasedMatcher matcher;
+  std::vector< cv::DMatch > matches;
+  matcher.match( descriptors_left, descriptors_right, matches );
+  double max_dist = 0; double min_dist = 100;
+
+  //-- Quick calculation of max and min distances between keypoints
+  for (int i = 0; i < descriptors_left.rows; i++) {
+    double dist = matches[i].distance;
+    if( dist < min_dist ) min_dist = dist;
+    if( dist > max_dist ) max_dist = dist;
+  }
+  std::cout << "Max distance is: " << max_dist << std::endl;
+  std::cout << "Min distance is: " << min_dist << std::endl;
+
+  //-- Draw only "good" matches (i.e. whose distance is less than 2*min_dist,
+  //-- or a small arbitary value ( 0.02 ) in the event that min_dist is very
+  //-- small)
+  //-- PS.- radiusMatch can also be used here.
+  std::vector< cv::DMatch > good_matches;
+  for (int i = 0; i < descriptors_left.rows; i++) {
+    if (matches[i].distance <= cv::max(5*min_dist, 0.02)) {
+      good_matches.push_back( matches[i]);
+    }
+  }
+  std::cout << "Number of good matches in frame: " << good_matches.size() << std::endl;
+
+  //-- Draw only "good" matches
+  for (auto it:good_matches) {
+    DrawLandmarkCorrespondance(left_img,keypoints_left[it.queryIdx].pt,right_img,keypoints_right[it.trainIdx].pt);
+  }
+
+  return 0;
+}
 
 int SolveBaProblem(std::string bal_file) {
   BALProblem bal_problem;
@@ -63,7 +130,7 @@ bool use_cerese_ba = true;
   std::cout << "Starting bundle_adjuster ..." << std::endl;
 
   ///----- Set up GUI.
-  pangolin::CreateGlutWindowAndBind("bundle_adjuster", 1600, 800);
+  pangolin::CreateGlutWindowAndBind("bundle_adjuster", 1250, 800);
 
   // Set up panel.
   const unsigned int panel_size = 180;
@@ -71,9 +138,6 @@ bool use_cerese_ba = true;
       .SetBounds(0, 1, 0, pangolin::Attach::Pix(panel_size));
   pangolin::Var<bool> ui_camera_follow("ui.Camera Follow", false, true);
   pangolin::Var<bool> ui_reset("ui.Reset", true, false);
-  pangolin::Var<bool> ui_show_vo_path("ui.Show VO Path", true, true);
-  pangolin::Var<bool> ui_show_ba_path("ui.Show BA Path", false, true);
-  pangolin::Var<bool> ui_show_ba_rel_path("ui.Show BA Rel Path", true, true);
   pangolin::Var<bool> ui_show_gt_path("ui.Show GT Path", true, true);
 
   // Set up container.
@@ -97,7 +161,7 @@ bool use_cerese_ba = true;
   std::vector<Sophus::SE3d>& path_gt_vec = gl_path_gt.GetPathRef();
 
   // Add grid.
-  SceneGraph::GLGrid gl_grid(150, 1);
+  SceneGraph::GLGrid gl_grid(100, 1);
   gl_graph.AddChild(&gl_grid);
 
   pangolin::View view_3d;
@@ -109,7 +173,21 @@ bool use_cerese_ba = true;
   view_3d.SetHandler(new SceneGraph::HandlerSceneGraph(gl_graph, stacks3d))
       .SetDrawFunction(SceneGraph::ActivateDrawFunctor(gl_graph, stacks3d));
 
-  container.AddDisplay(view_3d);
+  // Add all subviews to container.
+  SceneGraph::ImageView left_image_view;
+  left_image_view.SetAspect(640.0 / 480.0);
+  container.AddDisplay(left_image_view);
+
+  SceneGraph::ImageView right_image_view;
+  right_image_view.SetAspect(640.0 / 480.0);
+  container.AddDisplay(right_image_view);
+  //Draw line
+
+  pangolin::GlState glstate;
+  glstate.glEnable(GL_LINES);
+  glstate.glLineWidth(1.0f);
+
+//  container.AddDisplay(view_3d);
 
   // GUI aux variables.
   bool paused = true;
@@ -211,6 +289,20 @@ bool use_cerese_ba = true;
   Trv.so3() = calibu::RdfRobotics;
   // Sophus::SE3d Tic = rig.t_wc_[0] * Trv;
 
+  //Draw Images
+  cv::Mat img_1 = cv::imread( "/Users/Sina/rpg/classproject/bundle_adjuster/left_1.png", CV_LOAD_IMAGE_COLOR );
+  cv::Mat img_2 = cv::imread( "/Users/Sina/rpg/classproject/bundle_adjuster/left_2.png", CV_LOAD_IMAGE_COLOR );
+  cv::Point p1;
+  p1.x = 200;
+  p1.y = 10;
+  cv::Point p2;
+  p2.x = 400;
+  p2.y = 450;
+//  cv::line(img_1,p1,p2,cv::Scalar(0,255,0),1);
+//  cv::circle(img_1,p1,100,cv::Scalar(255,0,0),5,8,0);
+  FindAndDrawMatches(img_1,img_2);
+  left_image_view.SetImage(img_1.data,img_1.cols,img_1.rows,GL_RGB,GL_RGB,GL_UNSIGNED_BYTE);
+  right_image_view.SetImage(img_2.data,img_2.cols,img_2.rows,GL_RGB,GL_RGB,GL_UNSIGNED_BYTE);
   /////////////////////////////////////////////////////////////////////////////
   ///---- MAIN LOOP
   ///
